653005cbc672cb0b87a923e797e83d9e5008509b
Zzzzoey26 <152179344+Zzzzoey26@users.noreply.github.com>
2023-12-01 15:30:47 +0800

    newdata.sql

--- src/newdata.sql
+++ src/newdata.sql
@@ -1,57 +1,119 @@
-Customer:
-INSERT INTO Customer VALUES (1, 'John Doe', 12345, '123 Main St', 'Credit Card');
-INSERT INTO Customer VALUES (2, 'Jane Smith', 67890, '456 Elm St', 'PayPal');
-INSERT INTO Customer VALUES (3, 'Michael Johnson', 54321, '789 Oak St', 'Credit Card');
-INSERT INTO Customer VALUES (4, 'Emily Davis', 98765, '321 Pine St', 'Venmo');
-INSERT INTO Customer VALUES (5, 'David Wilson', 13579, '654 Cedar St', 'PayPal');
-INSERT INTO Customer VALUES (6, 'John Doe', 12345, '123 Main St', 'Credit Card');
-INSERT INTO Customer VALUES (7, 'John Doe', 12345, '123 Main St', 'Credit Card');
-INSERT INTO Customer VALUES (8, 'John Doe', 12345, '123 Main St', 'Credit Card');
-INSERT INTO Customer VALUES (9, 'John Doe', 12345, '123 Main St', 'Credit Card');
-INSERT INTO Customer VALUES (10, 'John Doe', 12345, '123 Main St', 'Credit Card');
+//customer 
+INSERT INTO Customer VALUES (1, 'John Doe', 12345, '123 Main St, Anytown', 'Credit Card');
+INSERT INTO Customer VALUES (2, 'Jane Smith', 67890, '456 Oak Dr, Somecity', 'Debit Card');
+INSERT INTO Customer VALUES (3, 'Robert Johnson', 11223, '789 Pine Rd, Othertown', 'Paypal');
+INSERT INTO Customer VALUES (4, 'Alice Williams', 44556, '111 Tree St, Cityville', 'Credit Card');
+INSERT INTO Customer VALUES (5, 'Bob Brown', 77889, '222 River Rd, Townville', 'Debit Card');
+INSERT INTO Customer VALUES (6, 'Charlie Davis', 33221, '333 Hill Ln, Villecity', 'Paypal');
+INSERT INTO Customer VALUES (7, 'David Miller', 99331, '444 Peak Pl, Villeton', 'Credit Card');
+INSERT INTO Customer VALUES (8, 'Eva Garcia', 66332, '555 Beach Blvd, Coastown', 'Debit Card');
+INSERT INTO Customer VALUES (9, 'Frank Wilson', 33445, '666 Lake Ln, Laketon', 'Paypal');
+INSERT INTO Customer VALUES (10, 'Grace Martinez', 99884, '777 Star St, Skyville', 'Credit Card');
 
-Merchant:
-INSERT INTO VALUES (101, 'Merchant A', 1001)
-INSERT INTO VALUES (102, 'Merchant B', 1002);
-INSERT INTO VALUES (103, 'Merchant C', 1003);
-INSERT INTO VALUES (104, 'Merchant D', 1004);
-INSERT INTO VALUES (105, 'Merchant E', 1005);
+// merchant
+INSERT INTO Merchant1 VALUES (1, 'Apple', 101);
+INSERT INTO Merchant1 VALUES (2, 'Apple', 102);
+INSERT INTO Merchant1 VALUES (3, 'XIAOMI', 103);
+INSERT INTO Merchant1 VALUES (4, 'XIOAMI', 104);
+INSERT INTO Merchant1 VALUES (5, 'HUAWEI', 105);
+INSERT INTO Merchant1 VALUES (6, 'Apple', 106);
+INSERT INTO Merchant1 VALUES (7, 'HUAWEI', 107);
+INSERT INTO Merchant1 VALUES (8, 'XIAOMI', 108);
+INSERT INTO Merchant1 VALUES (9, 'Apple', 109);
+INSERT INTO Merchant1 VALUES (10, 'HUAWEI', 110);
 
-Product:
-INSERT INTO VALUES (1001, 'Product A', 9.99, 'Electronics', 101, 'Brand X', 50, 100);
-INSERT INTO VALUES (1002, 'Product B', 19.99, 'Clothing', 102, 'Brand Y', 30, 50);
-INSERT INTO VALUES (1003, 'Product C', 14.99, 'Home Decor', 103, 'Brand Z', 20, 80);
-INSERT INTO VALUES (1004, 'Product D', 7.99, 'Electronics', 104, 'Brand X', 15, 120);
-INSERT INTO VALUES (1005, 'Product E', 12.99, 'Clothing', 105, 'Brand Y', 10, 70);
-INSERT INTO VALUES (1006, 'Product F', 8.99, 'Home Decor', 101, 'Brand Z', 25, 90);
-INSERT INTO VALUES (1007, 'Product G', 16.99, 'Electronics', 102, 'Brand X', 40, 60);
-INSERT INTO VALUES (1008, 'Product H', 14.99, 'Clothing', 103, 'Brand Y', 30, 110);
-INSERT INTO VALUES (1009, 'Product I', 10.99, 'Home Decor', 104, 'Brand Z', 20, 100);
-INSERT INTO VALUES (1010, 'Product J', 16.99, 'Electronics', 105, 'Brand X', 15, 80);
+//product
+INSERT INTO Product VALUES (101, 'phone', 19.99, 1, 1, 'apple', 100, 50;
+INSERT INTO Product VALUES (102, 'ipad', 29.99, 2, 2, 'HUWEI', 200, 60);
+INSERT INTO Product VALUES (103, 'computer', 39.99, 3, 2, 'XIAOMI', 150, 70);
+INSERT INTO Product VALUES (104, 'earphone', 49.99, 4, 2, 'apple', 180, 80);
+INSERT INTO Product VALUES (105, 'mouse', 59.99, 2, 5, 'HUWEI', 170, 90);
+INSERT INTO Product VALUES (106, 'phone', 89.99, 3, 6, 'XIAOMI', 120, 30);
+INSERT INTO Product VALUES (107, 'ipad', 99.99, 1, 7, 'HUWEI', 130, 40);
+INSERT INTO Product VALUES (108, 'mouse', 79.99, 2, 8, 'XIAOMI', 140, 50);
+INSERT INTO Product VALUES (109, 'computer', 69.99, 9, 5, 'HUWEI', 150, 60);
+INSERT INTO Product VALUES (110, 'ipad', 59.99, 1, 10, 'apple', 160, 70);
 
-shopping_cart:
-INSERT INTO VALUES (1, 1001, 'Product A', 9.99, 2, 19.98);
-INSERT INTO VALUES (1, 1003, 'Product C', 14.99, 1, 14.99);
-INSERT INTO VALUES (2, 1002, 'Product B', 19.99, 3, 59.97);
-INSERT INTO VALUES (2, 1005, 'Product E', 12.99, 2, 25.98);
-INSERT INTO VALUES (3, 1004, 'Product D', 7.99, 5, 39.95);
+//shopping_cart
+INSERT INTO shopping_cart VALUES (1, 101, 'Phone', 19.99, 2, 19.99*2);
+INSERT INTO shopping_cart VALUES (2, 102, 'HUAWEI', 29.99, 1, 29.99*1);
+INSERT INTO shopping_cart VALUES (3, 103, 'ipad', 39.99, 3, 39.99*3);
+INSERT INTO shopping_cart VALUES (4, 104, 'HUAWEI', 49.99, 2, 49.99*2);
+INSERT INTO shopping_cart VALUES (2, 107, 'ipad', 59.99, 1, 59.99*1);
+INSERT INTO shopping_cart VALUES (6, 101, 'Phone', 19.99, 2, 19.99*2);
+INSERT INTO shopping_cart VALUES (7, 102, 'HUAWEI', 29.99, 1, 29.99*1);
+INSERT INTO shopping_cart VALUES (8, 103, 'ipad', 39.99, 3, 39.99*3);
+INSERT INTO shopping_cart VALUES (6, 104, 'HUAWEI', 49.99, 2, 49.99*2);
+INSERT INTO shopping_cart VALUES (10, 109, 'ipad', 59.99, 1, 59.99*1);
 
-Order:
-INSERT INTO VALUES (1, 1, TO_DATE('2023-11-01', 'YYYY-MM-DD'), 1001, 2, 19.98, 'Credit Card', '123 Main St');
-INSERT INTO VALUES (2, 1, TO_DATE('2023-11-05', 'YYYY-MM-DD'), 1003, 1, 14.99, 'PayPal', '123 Main St');
-INSERT INTO VALUES (3, 2, TO_DATE('2023-11-03', 'YYYY-MM-DD'), 1002, 3, 59.97, 'Credit Card', '456 Elm St');
-INSERT INTO VALUES (4, 2, TO_DATE('2023-11-06', 'YYYY-MM-DD'), 1005, 2, 25.98, 'Credit Card', '456 Elm St');
-INSERT INTO VALUES (5, 3, TO_DATE('2023-11-02', 'YYYY-MM-DD'), 1004, 5, 39.95, 'PayPal', '789 Oak St');
+//orders
+INSERT INTO Orders VALUES (1, 1, TO_DATE('2023-11-01', 'YYYY-MM-DD'), 101, 2, 19.99*2, 'Credit Card', '123 Main St, City');
+INSERT INTO Orders VALUES (2, 1, TO_DATE('2023-10-05', 'YYYY-MM-DD'), 101, 2, 19.99*2, 'Credit Card', '123 Main St, City');
+INSERT INTO Orders VALUES (3, 1, TO_DATE('2023-06-03', 'YYYY-MM-DD'), 102, 1, 29.99*1, 'Credit Card', '123 Main St, City');
+INSERT INTO Orders VALUES (4, 2, TO_DATE('2023-08-01', 'YYYY-MM-DD'), 103, 3, 39.99*3, 'PayPal', '456 Maple Dr, City');
+INSERT INTO Orders VALUES (5, 2, TO_DATE('2023-04-10', 'YYYY-MM-DD'), 104, 2, 49.99*2, 'PayPal', '456 Maple Dr, City');
+INSERT INTO Orders VALUES (6, 9, TO_DATE('2023-03-01', 'YYYY-MM-DD'), 105, 1, 59.99*1, 'PayPal', '456 Maple Dr, City');
+INSERT INTO Orders VALUES (7, 3, TO_DATE('2023-08-26', 'YYYY-MM-DD'), 106, 2, 89.99*2, 'Debit Card', '789 Oak St, Cityj');
+INSERT INTO Orders VALUES (8, 3, TO_DATE('2023-01-01', 'YYYY-MM-DD'), 107, 3, 99.99*3, 'Debit Card', '789 Oak St, City');
+INSERT INTO Orders VALUES (9, 4, TO_DATE('2023-09-26', 'YYYY-MM-DD'), 108, 1, 79.99*1, 'PayPal', '321 Pine Dr, City');
+INSERT INTO Orders VALUES (10, 4, TO_DATE('2023-03-01', 'YYYY-MM-DD'), 109, 2, 69.99*2, 'PayPal', '321 Pine Dr, City');
 
-report:
-INSERT INTO VALUES (DATE '2023-11-29', 6, 106, 'Merchant F', 1011, 16.99, 'Brand Y', 15);
-INSERT INTO VALUES (DATE '2023-11-29', 7, 107, 'Merchant G', 1012, 13.99, 'Brand Z', 25);
-INSERT INTO VALUES (DATE '2023-11-30', 8, 108, 'Merchant H', 1013, 9.99, 'Brand X', 35);
-INSERT INTO VALUES (DATE '2023-11-30', 9, 109, 'Merchant I', 1014, 12.99, 'Brand Y', 20);
-INSERT INTO VALUES (DATE '2023-11-30', 10, 110, 'Merchant J', 1015, 7.99, 'Brand Z', 50);
-INSERT INTO VALUES (DATE '2023-11-30', 6, 106, 'Merchant F', 1016, 11.99, 'Brand X', 40);
-INSERT INTO VALUES (DATE '2023-11-30', 7, 107, 'Merchant G', 1017, 18.99, 'Brand Y', 30);
-INSERT INTO VALUES (DATE '2023-11-30', 8, 108, 'Merchant H', 1018, 14.99, 'Brand Z', 10);
-INSERT INTO VALUES (DATE '2023-11-30', 9, 109, 'Merchant I', 1019, 10.99, 'Brand X', 30);
-INSERT INTO VALUES (DATE '2023-11-30', 10, 110, 'Merchant J', 1020, 16.99, 'Brand Y', 20);
+//report
+INSERT INTO report VALUES (DATE '2023-11-29', 1, 1, 'Merchant 1', 101, 19.99, 'Apple', 100);
+INSERT INTO report VALUES (DATE '2023-01-01', 2, 2, 'Merchant 2', 102, 29.99, 'HUWEI', 200);
+INSERT INTO report VALUES (DATE '2023-12-01', 3, 3, 'Merchant 3', 103, 39.99, 'XIAOMI', 300);
+INSERT INTO report VALUES (DATE '2023-03-01', 4, 4, 'Merchant 4', 104, 49.99, 'HUWEI', 400);
+INSERT INTO report VALUES (DATE '2023-04-01', 5, 5, 'Merchant 5', 105, 59.99, 'Apple', 500);
+INSERT INTO report VALUES (DATE '2023-05-01', 6, 1, 'Merchant 6', 106, 69.99, 'XIAOMI', 600);
+INSERT INTO report VALUES (DATE '2023-07-01', 7, 2, 'Merchant 7', 107, 79.99, 'HUWEI', 700);
+INSERT INTO report VALUES (DATE '2023-06-01', 8, 3, 'Merchant 8', 108, 89.99, 'Apple', 800);
+INSERT INTO report VALUES (DATE '2023-09-01', 9, 4, 'Merchant 9', 109, 99.99, 'XIAOMI', 900);
+INSERT INTO report VALUES (DATE '2023-08-01', 10, 5, 'Merchant 10', 110, 109.99, 'HUWEI', 1000);
 
+// Contain 
+INSERT INTO Contain VALUES (101, 1);
+INSERT INTO Contain VALUES (102, 2);
+INSERT INTO Contain VALUES (103, 3);
+INSERT INTO Contain VALUES (104, 4);
+INSERT INTO Contain VALUES (105, 5);
+INSERT INTO Contain VALUES (106, 6);
+INSERT INTO Contain VALUES (107, 7);
+INSERT INTO Contain VALUES (108, 8);
+INSERT INTO Contain VALUES (109, 9);
+INSERT INTO Contain VALUES (110, 10);
+
+//proivde
+INSERT INTO Provide VALUES (101, 1);
+INSERT INTO Provide VALUES (102, 2);
+INSERT INTO Provide VALUES (103, 3);
+INSERT INTO Provide VALUES (104, 4);
+INSERT INTO Provide VALUES (105, 5);
+INSERT INTO Provide VALUES (106, 6);
+INSERT INTO Provide VALUES (107, 7);
+INSERT INTO Provide VALUES (108, 8);
+INSERT INTO Provide VALUES (109, 9);
+INSERT INTO Provide VALUES (110, 10);
+
+//Purchase
+INSERT INTO Purchase VALUES (101, 1);
+INSERT INTO Purchase VALUES (102, 2);
+INSERT INTO Purchase VALUES (103, 3);
+INSERT INTO Purchase VALUES (104, 4);
+INSERT INTO Purchase VALUES (105, 5);
+INSERT INTO Purchase VALUES (106, 6);
+INSERT INTO Purchase VALUES (107, 7);
+INSERT INTO Purchase VALUES (108, 8);
+INSERT INTO Purchase VALUES (109, 9);
+INSERT INTO Purchase VALUES (110, 10);
+
+//Communicate
+INSERT INTO Communicate VALUES (101, 1);
+INSERT INTO Communicate VALUES (102, 2);
+INSERT INTO Communicate VALUES (103, 3);
+INSERT INTO Communicate VALUES (104, 4);
+INSERT INTO Communicate VALUES (105, 5);
+INSERT INTO Communicate VALUES (106, 6);
+INSERT INTO Communicate VALUES (107, 7);
+INSERT INTO Communicate VALUES (108, 8);
+INSERT INTO Communicate VALUES (109, 9);
+INSERT INTO Communicate VALUES (110, 10);
